0633e85214cba89cdf82b80806bf7363c3619fab
Willy <wacb14@gmail.com>
2025-04-19 16:01:53 -0500

    Pass input parameters to a view

--- src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/ExampleController.java
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/ExampleController.java
@@ -1,13 +1,16 @@
 package com.wacb14.spring.firstapp.spring_boot_firstapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExampleController {
     @GetMapping("/server_test")
-    public String info() {
+    public String info(Model model) {
+        model.addAttribute("Title", "Server's loading...");
+        model.addAttribute("Content", "Server's online!");
+        model.addAttribute("IP", "192.168.1.10");
         return "server_test";
     }
-
 }
