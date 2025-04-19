e72073aceb07fa65601ab64f436ed9c329e79c8c
Willy <wacb14@gmail.com>
2025-04-19 15:27:59 -0500

    Initial commit

---
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/SpringBootFirstappApplication.java
@@ -0,0 +1,13 @@
+package com.wacb14.spring.firstapp.spring_boot_firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wacb14/spring/firstapp/spring_boot_firstapp/SpringBootFirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wacb14.spring.firstapp.spring_boot_firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
