a0e563d667dc0917dae09631d325b0e9270c922f
Willy <wacb14@gmail.com>
2025-04-21 18:37:52 -0500

    Directive if-unless

--- src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/ExampleController.java
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/ExampleController.java
@@ -3,6 +3,7 @@ package com.wacb14.spring.firstapp.spring_boot_firstapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.wacb14.spring.firstapp.spring_boot_firstapp.models.dto.User;
 
 @Controller
 public class ExampleController {
@@ -13,4 +14,14 @@ public class ExampleController {
         model.addAttribute("IP", "192.168.1.10");
         return "server_test";
     }
+
+    @GetMapping("/user-profile")
+    public String userProfile(Model model) {
+        User user = new User();
+        user.setUser("pepelucho");
+        user.setPassword(null);
+        model.addAttribute(user);
+        return "user-profile";
+    }
+
 }
