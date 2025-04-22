611b0a5df035670c422edd48ce3575c783807718
Willy <wacb14@gmail.com>
2025-04-21 19:02:03 -0500

    Directive each and decorator modelattribute

--- src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/ExampleController.java
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/ExampleController.java
@@ -1,8 +1,14 @@
 package com.wacb14.spring.firstapp.spring_boot_firstapp.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.wacb14.spring.firstapp.spring_boot_firstapp.models.Employee;
 import com.wacb14.spring.firstapp.spring_boot_firstapp.models.dto.User;
 
 @Controller
@@ -22,6 +28,20 @@ public class ExampleController {
         user.setPassword(null);
         model.addAttribute(user);
         return "user-profile";
+    }
+
+    @GetMapping("/employees-list")
+    public String employeesList() {
+        return "employees-list";
+    }
+
+    @ModelAttribute("Employees")
+    public List<Employee> EmployeesList() {
+        return Arrays.asList(
+                new Employee(1, "Pepe Gonzales", "Las Lomas 132", 45),
+                new Employee(2, "Francesca Maldini", "Las Lomas 133", 65),
+                new Employee(3, "Miguel Ignacio De las Casas", "Las Lomas 134", 50),
+                new Employee(4, "Fernanda De las Casas", "Las Lomas 133", 23));
     }
 
 }
