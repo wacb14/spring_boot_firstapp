b216f2f5ef83602c40149be2ec44f6d3f129f3bc
Willy <wacb14@gmail.com>
2025-04-23 18:14:57 -0500

    Annotation @value to get values from application properties

--- src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/PathVariableController.java
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/PathVariableController.java
@@ -2,9 +2,10 @@ package com.wacb14.spring.firstapp.spring_boot_firstapp.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.wacb14.spring.firstapp.spring_boot_firstapp.models.dto.Parameter;
-
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/variable")
 public class PathVariableController {
+    // These values are brought from the file application.properties
+    @Value("${config.name}")
+    private String name;
+    @Value("${config.age}")
+    private int age;
+    @Value("${config.colors}")
+    String[] colors;
+
+    @GetMapping("/values")
+    public Map<String, Object> values() {
+        Map<String, Object> values = new HashMap<>();
+        values.put("name", name);
+        values.put("age", age);
+        values.put("colors", colors);
+        return values;
+    }
 
     @GetMapping("/page/{message}")
     public Parameter page(@PathVariable String message) {
