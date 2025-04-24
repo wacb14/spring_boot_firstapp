23938bf052eb1a8359f513bf7f98f192198f6f2a
Willy <wacb14@gmail.com>
2025-04-23 19:18:07 -0500

    Controller for product

---
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/controllers/Product_Controller.java
@@ -0,0 +1,29 @@
+package com.wacb14.spring.firstapp.spring_boot_firstapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wacb14.spring.firstapp.spring_boot_firstapp.models.Product;
+import com.wacb14.spring.firstapp.spring_boot_firstapp.services.Product_Service;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api")
+public class Product_Controller {
+    Product_Service products = new Product_Service();
+
+    @GetMapping("/getAllProducts")
+    public List<Product> getAllProducts() {
+        return products.getAll();
+    }
+
+    @GetMapping("/getById")
+    public Product getById(@RequestParam long id) {
+        return products.getById(id);
+    }
+
+}
