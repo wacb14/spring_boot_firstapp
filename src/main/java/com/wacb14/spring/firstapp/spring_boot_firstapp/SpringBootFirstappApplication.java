782c48662ef226563fa5b4830694bf41fc6de452
Willy <wacb14@gmail.com>
2025-05-02 11:09:23 -0500

    Initial config for Hybernate

--- src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/SpringBootFirstappApplication.java
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/SpringBootFirstappApplication.java
@@ -1,13 +1,18 @@
 package com.wacb14.spring.firstapp.spring_boot_firstapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootFirstappApplication {
+public class SpringBootFirstappApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstappApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 	}
 
 }
