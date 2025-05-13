58713edb469a938c0e4e6b87b851f45eb4441539
Willy <wacb14@gmail.com>
2025-05-13 10:45:27 -0500

    SecurityClass basic authetication with user and password for http requests with no csrf

---
+++ src/main/java/com/wacb14/spring/firstapp/spring_boot_firstapp/security/SecurityClass.java
@@ -0,0 +1,18 @@
+package com.wacb14.spring.firstapp.spring_boot_firstapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityClass {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(httpBasic -> {
+                });
+        return http.build();
+    }
+}
